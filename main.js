7694043b623a23693840e3a6abc07555edf95493
Kristan Panton <kristan.panton@gmail.com>
2020-11-09 21:22:32 -0500

    updated content scroll

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 let tsInfo = ["Open source, compiled language", "Built on JavaScript", "Created and developed by Microsoft", "Offers gradual adoption"];
 
 $(function () {
+    // Updates the inner html of span.content with text from tsInfo on click
     let $displayLeftBtn = $(".display-container span.left");
     let $displayRightBtn = $(".display-container span.right");
+
+    // The span that gets updated
     let $displayContent = $(".display-container span.content");
 
     let displayIndex = 0;
     $displayContent.text(tsInfo[displayIndex]);
     $displayLeftBtn.click(function () {
-        displayIndex = --displayIndex % tsInfo.length;
+        // Updates and wraps the current index if the current index is negative
+        --displayIndex;
+        if (displayIndex < 0)
+            displayIndex = tsInfo.length - 1;
+        // displayIndex = --displayIndex % tsInfo.length;
         $displayContent.text(tsInfo[displayIndex]);
     });
 
@@ -17,14 +24,21 @@ $(function () {
         $displayContent.text(tsInfo[displayIndex]);
     });
 
+    // Changes the visible image on click
+    // Wanted to change the src attribute, but it was too slow
     let $detailLeftBtn = $(".detail-container span.left");
     let $detailRightBtn = $(".detail-container span.right");
     let $detailImg = $(".detail-container img.responsive");
     let detailIndex = 0;
 
     $detailLeftBtn.click(function () {
-        $detailImg.eq(detailIndex).removeClass("visible");
-        detailIndex = --detailIndex % $detailImg.length;
+        // Removes visiblity from previous image
+        $detailImg.eq(detailIndex).removeClass("visible"); // .visible { display: initial }
+        --detailIndex;
+        if (detailIndex < 0)
+            detailIndex = tsInfo.length - 1;
+        // detailIndex = --detailIndex % $detailImg.length;
+        // Adds visibility to current immage
         $detailImg.eq(detailIndex).addClass("visible");
     });
 
